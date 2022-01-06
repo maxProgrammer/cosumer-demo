b5e18fc6d1e5eb9692c54ddc43efabf13122fd6f
maxProgrammer <maxwneto.poo@gmail.com>
2022-01-06 15:16:17 -0300

    consumer demo using lambda

---
+++ src/com/maxprogrammer/util/PriceUpdate.java
@@ -0,0 +1,13 @@
+package com.maxprogrammer.util;
+
+import com.maxprogrammer.entities.Product;
+
+import java.util.function.Consumer;
+
+public class PriceUpdate implements Consumer<Product> {
+
+    @Override
+    public void accept(Product p) {
+        p.setPrice(p.getPrice() * 1.1);
+    }
+}
